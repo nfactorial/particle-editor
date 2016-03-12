9e64c4a27c14ab7d12003a8bacdd0b50037dbba3
nfactorial <nfactorial@btopenworld.com>
2016-03-12 23:58:29 +0000

    Updated gulpfile with some tasks.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
+"use strict";
+
 var gulp = require('gulp'),
-    babel = require('gulp-babel' );
+    babel = require('gulp-babel' ),
+    sass = require('gulp-sass'),
+    del = require('del');
 
 var sourceFolder = './web-dev', // Folder containing development code
     buildFolder = './build',    // Folder for temporary files during build process
@@ -9,3 +13,30 @@ gulp.task('default', [
     'clean:build'
 ]);
 
+
+/**
+ * Deletes all files inside the build folder.
+ */
+gulp.task('clean:build', function() {
+    return del.sync([buildFolder + '/**/*'])
+});
+
+
+/**
+ * Compiles all .sass files into raw css.
+ */
+gulp.task('build:sass', function() {
+    return gulp.src(sourceFolder + '/**/!(_)*.sass')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest(distFolder));
+});
+
+
+/**
+ * Compiles all jsx files into ES5 source files.
+ */
+gulp.task('build:js', function() {
+    return gulp.src(sourceFolder + '/**/*!(-compiled).jsx')
+        .pipe(babel())
+        .pipe(gulp.dest(buildFolder));
+});
